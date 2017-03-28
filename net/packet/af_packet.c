jrfastab/af_packet_v4

commit eb4f4c0ae3d5df60b4b3ea3c469d65f5e92444ea

af_packet V4: Write used descriptors backwards
Writing descriptors backwards helps to improve performance when
the user space application is waiting for new packets.

Signed-off-by: Magnus Karlsson <magnus.karlsson@intel.com>

net/packet/af_packet.c
@@ -293,11 +293,13 @@ int tp4ring_add_used(struct tpacket4_queue_kernel *q,
 
 	smp_wmb();
 
-	for (i = 0; i < dcnt; i++) {
-		unsigned int idx = (q->used_idx++) & (q->ring_size - 1);
+	for (i = dcnt; i >= 0; i--) {
+		unsigned int idx = (q->used_idx + i) & (q->ring_size - 1);
 
 		q->vring[idx].flags = d[i].flags & ~DESC_HW;
 	}
+	q->used_idx += dcnt;
+
 	return 0;
 }
 EXPORT_SYMBOL(tp4ring_add_used);

commit 8626074d370f211b85d18963896e5dd642f066a9

packet: Fix off-by-one error in for-loop
eb4f4c0ae3d5 ("af_packet V4: Write used descriptors backwards")
introduced an off-by-one bug.

Fixes: eb4f4c0ae3d5 ("af_packet V4: Write used descriptors backwards")
Signed-off-by: Björn Töpel <bjorn.topel@intel.com>

net/packet/af_packet.c
@@ -293,7 +293,8 @@ int tp4ring_add_used(struct tpacket4_queue_kernel *q,
 
 	smp_wmb();
 
-	for (i = dcnt; i >= 0; i--) {
+	/* NOTE: We're not checking dcnt against zero... */
+	for (i = dcnt - 1; i >= 0; i--) {
 		unsigned int idx = (q->used_idx + i) & (q->ring_size - 1);
 
 		q->vring[idx].flags = d[i].flags & ~DESC_HW;